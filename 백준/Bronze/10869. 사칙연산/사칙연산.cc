4fe4b5145909461389a9d55ed8a593f9547e24ef
Tivm <109393040+liiililiil@users.noreply.github.com>
2024-09-04 23:58:27 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void){
+    int a,b;
+
+    scanf("%d %d", &a, &b);
+    //받기
+
+    printf("%d \n", a+b);
+    printf("%d \n", a-b);
+    printf("%d \n", a*b);
+    printf("%d \n", a/b);
+    printf("%d \n", a%b);
+    //추울울울력
+
+}
