8c94ef424b0be132b907ee178540b1fe81ed3e01
Tivm <109393040+liiililiil@users.noreply.github.com>
2024-10-07 01:23:50 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 304 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    int l,a,b;
+    scanf("%d",&l);
+    
+    for(int i = 0; i <l; i++){
+        scanf("%d %d",&a,&b);
+        printf("%d \n",a+b);
+    }
+
+    
+}
