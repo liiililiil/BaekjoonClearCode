b337435fad0761903678f0552f2f520aa5d60a99
Tivm <109393040+liiililiil@users.noreply.github.com>
2024-10-31 04:04:25 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+//제발 변수 초기화하자
+#include <stdio.h>
+int main(void){
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+
+
+    
+}
